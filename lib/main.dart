96db43406c694d8c1bf3bd9c153d240d2dbd2ec1
Zubnormal Numberz <33485885+zubnormalnumberz@users.noreply.github.com>
2021-01-14 15:07:15 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Tooltip Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Tooltip(
+                message: 'My Account',
+                child: FlatButton(
+                  onPressed: () {
+                    print("kaixooooo");
+                  },
+                  child: Icon(
+                    Icons.account_box,
+                    size: 50,
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
